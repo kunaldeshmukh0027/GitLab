7dc22a18609aace515edadc571df3a6b7b84ff4f
kunaldeshmukh0027 <kunaldeshmukh2709@gmail.com>
2026-08-10 10:40:38 +0530

    Total Marks.py
    Added OS marks

--- marks.py
+++ marks.py
@@ -2,5 +2,7 @@ print("*Enter Student Marks*")
 java=int(input("Enter Java Marks: "))
 FDS=int(input("Enter Data Science Marks: "))
 Gitlab=int(input("Enter GitLab Marks: "))
+os=int(input("Enter Operating System Marks: "))
 print(f"1.Java Marks:{java}\n2.Data Science Marks:{FDS}\n3.GitLab Marks:{Gitlab}")
+print("4.Operating System marks: ")
 
