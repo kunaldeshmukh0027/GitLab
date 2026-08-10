2ab9716d438904723306eb9e37f0dd8a01a464a0
kunaldeshmuk0027 <kunaldeshmukh2709@gmail.com>
2026-08-10 10:07:48 +0530

    Student and there marks

---
+++ Student.py
@@ -0,0 +1,6 @@
+name=input("Enter Student Name: ")
+roll_no=int(input("Enter Student roll no: "))
+course=input("Enter Student Course: ")
+print("*Student Detail*")
+print(f"1.Student Name:{name}\n 2.Student Roll Number:{roll_no}\n 3.Student Course:{course}")
+
---
+++ marks.py
@@ -0,0 +1,6 @@
+print("*Enter Student Marks*")
+java=int(input("Enter Java Marks: "))
+FDS=int(input("Enter Data Science Marks: "))
+Gitlab=int(input("Enter GitLab Marks: "))
+print(f"1.Java Marks:{java}\n2.Data Science Marks:{FDS}\n3.GitLab Marks:{Gitlab}")
+
